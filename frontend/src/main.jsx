50837b9bed471becf15abc0aba261493c218b5cd
Muhammed Musa <sam@overthetopseo.com>
2025-08-04 11:46:47 +0100

    Update frontend branding and dependencies
    - Changed the favicon from 'vite.svg' to 'CrossFi-chain.png' and updated the page title to 'VPay' in index.html for rebranding.
    - Added new dependencies: '@rainbow-me/rainbowkit', '@tanstack/react-query', 'react-toastify', 'viem', and 'wagmi' in package.json and package-lock.json to enhance functionality.
    - Cleaned up App.jsx by removing unused imports and components for improved code clarity.
    - Integrated ToastContainer from 'react-toastify' in main.jsx for better user notifications.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import "@rainbow-me/rainbowkit/styles.css";
 import { WagmiConfigProvider } from "@/provider/Wagmi";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <WagmiConfigProvider>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <App />
     </WagmiConfigProvider>
   </StrictMode>
