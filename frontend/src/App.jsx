5ea39a47d2500323e03a01320f64bea82b76c145
Muhammed Musa <sam@overthetopseo.com>
2025-08-04 12:07:52 +0100

    Enhance frontend configuration and dependencies
    - Updated .gitignore to include .env files for better environment management.
    - Added new dependencies '@dicebear/collection' and '@dicebear/core' in package.json and package-lock.json to support avatar generation features.
    - Modified Vite configuration to include path handling and defined process environment variables for improved compatibility.
    - Updated App component to integrate ConnectButton from '@rainbow-me/rainbowkit' for enhanced user interaction.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit"
 
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <ConnectButton/>
+    </div>
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +10,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  define: {
+    "process.env": {},
   },
 });
